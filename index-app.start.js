e82284e356de84ca27a42f564877708902ddcf5f
maayanAN <maayanangel@gmail.com>
2016-11-29 16:20:58 +0200

    start bookmarks project in angularJS

--- index-app.start.js
+++ index-app.start.js
@@ -13,25 +13,87 @@ angular.module('Index',[
     ];
 
     $scope.bookmarks = [
-        {"id": 0, "title": "100 years ago",
-            "category":$scope.categories[0].name},
-        {"id": 1, "title": "Shakespeare",
-            "category":$scope.categories[1].name},
-        {"id": 2, "title": "atoms",
-            "category":$scope.categories[2].name},
-        {"id": 3, "title": "multiplication board",
-            "category":$scope.categories[3].name}
+        {
+            "id": 0, "title": "100 years ago",
+            "category": $scope.categories[0].name
+        },
+        {
+            "id": 1, "title": "Shakespeare",
+            "category": $scope.categories[1].name
+        },
+        {
+            "id": 2, "title": "atoms",
+            "category": $scope.categories[2].name
+        },
+        {
+            "id": 3, "title": "multiplication board",
+            "category": $scope.categories[3].name
+        }
     ];
 
     $scope.currentCategory = null;
     function setCurrentCategory(categotry) {
-       $scope.currentCategory = categotry;
+        $scope.currentCategory = categotry;
     }
-    
+
     function isCurrentCategory(categoty) {
         return $scope.currentCategory !== null && categoty.name === $scope.currentCategory.name;
-        
+
     }
+
     $scope.setCurrentCategory = setCurrentCategory;
     $scope.isCurrentCategory = isCurrentCategory;
+
+    function isCreating() {
+        resetBookmark();
+        return true;
+    }
+
+    function resetBookmark() {
+        $scope.newBookmark = {
+            title: '',
+            category: $scope.currentCategory
+
+        }
+
+    }
+
+    function createBookmark(bookmark) {
+        bookmark.id = $scope.bookmarks.length;
+        $scope.bookmarks.push(bookmark);
+
+        resetBookmark();
+
+    }
+
+    $scope.isCreating = isCreating;
+    $scope.createBookmark = createBookmark;
+    $scope.editedBookmark = null;
+    function setEditedBookmark(bookmark) {
+
+        $scope.editedBookmark = angular.copy(bookmark);
+    }
+
+    function updateBookmark(bookmark) {
+        var index = $scope.bookmarks.findIndex(function (b) {
+            return b.id == bookmark.id;
+        });
+    $scope.bookmarks[index].title = bookmark.title;
+    };
+
+    function isSelectedBookmark(bookmarkId) {
+        return $scope.editedBookmark !==null && $scope.editedBookmark.id === bookmarkId;
+
+    }
+    $scope.setEditedBookmark = setEditedBookmark;
+    $scope.updateBookmark = updateBookmark;
+    $scope.isSelectedBookmark = isSelectedBookmark;
+
+    function deleteBookmark(bookmark) {
+        _.remove($scope.bookmarks, function (b) {
+            return b.id== bookmark.id;
+
+        })
+    }
+    $scope.deleteBookmark = deleteBookmark;
 });
