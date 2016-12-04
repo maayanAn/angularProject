a78de9152d608461d14cd121854d88aafc7fdc1b
maayanAN <maayanangel@gmail.com>
2016-12-04 16:55:20 +0200

    add angular material design project- with icons

--- UserController.js
+++ UserController.js
@@ -4,7 +4,7 @@
 (function () {
     angular.module('users').controller('UserController', UserController);
 
-    function UserController(userService, $mdSidenav, $mdButton) {
+    function UserController(userService, $mdSidenav, $mdBottomSheet, $log) {
         var self = this;
 
         self.selected = null;
@@ -13,21 +13,51 @@
         self.toggleList = toggleUsersList;
         self.share = share;
 
-        userService.loadAllUsers().then(function (users) {
+        userService.loadAllUsers()
+            .then(setUsers, error);
+
+        function setUsers(users) {
             self.users = [].concat(users);
             self.selected = users[0];
-        });
+        }
 
-    }
+        function error(error) {
+            console.log(error);
+        }
 
-    function toggleUsersList() {
-        $mdSidenav('left').toggle();
+        function toggleUsersList() {
+            $mdSidenav('left').toggle();
 
-    }
+        }
 
-    function selectUser(user) {
-        //self.select = angular.isNumber(user) ? $scope.users
-        console.log('hello');
+        function selectUser(user) {
+            self.selected = user;
+            console.log('hello');
 
-    }
-});
+        }
+
+        function share(selectedUser) {
+            $mdBottomSheet.show({
+                controller: UserSheetController,
+                controllerAs: 'vm',
+                templateUrl: './bottomsheet.html',
+                parent: angular.element(document.querySelector('#content'))
+            });
+
+            function UserSheetController() {
+                this.user = selectedUser;
+                this.items = [
+                    {name: 'local_florist'},
+                    {name: 'local_airport'},
+                    {name: 'local_bar'},
+                    {name: 'pets'},
+                    {name: 'cloud_upload'}
+                ];
+                this.perforAction = function (action) {
+                    $mdBottomSheet.hide();
+
+                };
+            }
+        }
+    }})();
+
--- UserService.js
+++ UserService.js
@@ -10,25 +10,35 @@
         var users = [
             {
                 name: 'Danna Smith',
-                avatar: 'girl.svg',
+                avatar: 'svg/girl.svg',
                 content: 'Nero Claudius Caesar Augustus Germanicus (December 15, 37 – June 9, 68), born Lucius Domitius Ahenobarbus, also called Nero Claudius Caesar Drusus Germanicus, was the fifth and last Roman Emperor of the Julio-Claudian dynasty.'
             },
             {
                 name: 'Jhon Collins',
-                avatar: 'boy.svg',
+                avatar: 'svg/boy.svg',
                 content: 'Nero was adopted by his grand-uncle Claudius to become heir to the throne. As Nero Claudius Caesar Drusus, he succeeded to the throne on October 13, 54, following Claudius.'
             },
             {
                 name: 'Rayn Collings',
-                avatar: 'boy_2.svg',
+                avatar: 'svg/boy_2.svg',
                 content: 'The Rendille (also known as Rendille, Reendile, Rendili, Randali, Randile, and Randille[2]) are a Cushitic-speaking ethnic group inhabiting the northern Eastern Province of Kenya.'
             },
             {
                 name: 'Bella McAdams',
-                avatar: 'girl_2.svg',
+                avatar: 'svg/girl_2.svg',
                 content: 'The Rendille are believed to have originally migrated down into the Great Lakes area from Ethiopia in the more northerly Horn region, following southward population expansions by the Oromo and later the Somali.'
             }
-        ]
+        ];
+
+        this.loadAllUsers = function () {
+            return $q(function (resolve, reject) {
+                if (users){
+                    resolve(users);
+                }else {
+                    reject("Could not resolve users.");
+                }
+            });
+        }
 
     }
 })();
